c70e18b2e4fa5ecb3b43205ac90a6ce065cf2d13
fonix232 <fonix232@gmail.com>
2011-04-25 15:26:33 +0200

    Added new defconfig Fixed touchscreen (uses serial console instead of /dev/input events)

--- m1400.mk
+++ m1400.mk
@@ -3,9 +3,9 @@ PRODUCT_PACKAGES := $(THIRD_PARTY_APPS)
 $(call inherit-product,$(SRC_TARGET_DIR)/product/generic_x86.mk)
 
 PRODUCT_NAME := motion_m1400
-PRODUCT_DEVICE := m1400
+PRODUCT_DEVICE := M1400
 PRODUCT_MANUFACTURER := Motion
-#PRODUCT_PACKAGE_OVERLAYS := device/viewsonic/viewpad10/overlays
+PRODUCT_PACKAGE_OVERLAYS := device/motion/m1400/overlays
 
 TARGET_STRIP := 1 
 USE_SQUASHFS := 0
